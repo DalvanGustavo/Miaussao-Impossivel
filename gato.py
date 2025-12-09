82fe194ee081b8b292011931c5e8beb29d8bd89c
DalvanGustavo <dalvan.oliveira2004@gmail.com>
2025-12-09 16:18:10 -0300

    Criação da Classe Gato e implementando os Sprites

---
+++ gato.py
@@ -0,0 +1,29 @@
+import pygame
+class Gato(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.walk_sprites = []
+        for i in range(1,4):
+            self.walk_sprites.append(pygame.image.load(f"sprites/tile_{i}.png"))
+        self.current_sprite = 0
+        self.animation_speed = 0.15
+        self.flip = False
+        self.image = self.walk_sprites[0]
+        self.rect = self.image.get_rect()
+    def andar_direita(self):
+        self.flip = False
+        self.current_sprite += self.animation_speed
+        if self.current_sprite >= len(self.walk_sprites):
+            self.current_sprite = 0
+        self.image = self.walk_sprites[int(self.current_sprite)]
+    def andar_esquerda(self):
+        self.flip = True
+        self.current_sprite += self.animation_speed
+        if self.current_sprite >= len(self.walk_sprites):
+            self.current_sprite = 0
+        self.image = pygame.transform.flip(self.walk_sprites[int(self.current_sprite)], True, False)
+    def parado(self):
+        if self.flip:
+            self.image = pygame.transform.flip(self.walk_sprites[0], True, False)
+        else:
+            self.image = self.walk_sprites[0]
---
+++ main.py
@@ -0,0 +1,44 @@
+import pygame
+from gato import Gato
+pygame.init()
+screen = pygame.display.set_mode((600, 400))
+x = 100
+y = 100
+vel_y = 0
+gravidade = 0.8
+chao_y = 300
+clock = pygame.time.Clock()
+running = True
+todas_as_sprites = pygame.sprite.Group()
+cat = Gato()
+todas_as_sprites.add(cat)
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_RIGHT]:
+        x += 3
+        cat.andar_direita()
+    elif keys[pygame.K_LEFT]:
+        x -= 3
+        cat.andar_esquerda()
+    elif keys[pygame.K_UP]:
+        y -= 15
+    else:
+        cat.parado()
+    vel_y += gravidade
+    y += vel_y
+    if y >= chao_y:
+        y = chao_y
+        vel_y = 0
+    camera_x = x - 300   
+    camera_y = y - 200   
+    cat.rect.topleft = (x - camera_x, y - camera_y)
+    screen.fill((255, 255, 255))
+    pygame.draw.rect(screen, (0, 255, 0), (0 - camera_x, chao_y + 50 - camera_y, 1200, 100))  
+    todas_as_sprites.draw(screen)
+    todas_as_sprites.update()
+    pygame.display.flip()
+    clock.tick(60)
+pygame.quit()
